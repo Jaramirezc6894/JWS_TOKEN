c48964006d8425a85ba9677966a1844547293c66
ramirezcja <50533777+Jaramirezc6894@users.noreply.github.com>
2022-06-09 17:20:22 -0500

    Se añade la configuración para el JWT, así mismo los paramentros que vamos a recibir para formar el JWT, se esta resolviendo el poblema del archivo base.api/urls para que el pueda correr el proyecto sin problema alguno

--- api/base/api/views.py
+++ api/base/api/views.py
@@ -1,6 +1,32 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-def getRoustes(request) :
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, com, n_user, s_id):
+        token = super().get_token(com)
+	  	token = super().get_token(n_user)
+	  	token = super().get_token(s_id)
+
+        # Agregar valores personalizados
+        token['company'] = com.name
+        token['network_user'] = n_user.name
+	  	token['sessionid'] = s_id.name
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request) :
 	routes = [
 		'/api/token',
 		'/api/token/refresh',
@@ -8,4 +34,4 @@ def getRoustes(request) :
 		]
 
 
-	return JsonResponse(routes, safe = False)
+	return Response(routes)
